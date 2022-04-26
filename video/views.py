81aa11325c21a13e875418f555cef338b5f22925
Hrithik <hrithikpaul2001@gmail.com>
2022-04-26 21:55:39 +0530

    Initial commit

---
+++ video/views.py
@@ -0,0 +1,65 @@
+from urllib import request
+from django.shortcuts import render
+from agora_token_builder import RtcTokenBuilder
+from django.http import JsonResponse
+import random
+import time
+from .models import RoomMember
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+def view(request):
+    return render(request,"base.html")
+
+def room(request):
+    return render(request,"vc.html")
+
+def getToken(request):
+    appId = "ce1d815c81b94a06bd750ca697a576f2"
+    appCertificate = "bbaa4624fc7341f1b7d44aa0178305fe"
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600
+    currentTimeStamp = int(time.time())
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+
+    return JsonResponse({'name':data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+    name = member.name
+    return JsonResponse({'name':member.name}, safe=False)
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+    member = RoomMember.objects.get(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+    member.delete()
+    return JsonResponse('Member deleted', safe=False)
